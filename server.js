032e875d61f98e4a440b9665971b5a3d31794d45
B-SFS <silwadi35@gmail.com>
2022-02-23 16:41:27 +0200

    commiting

--- server.js
+++ server.js
@@ -1,74 +1,65 @@
 'use strict';
+// initelizing variables
+
+// Get express from node model
 const express = require("express");
-const data = require("./MovieData/data.json");
+
+// initializing my server
 const app = express();
-const Api = require("axios");
-const axios = axios();
-app.get('/' , moviesHandler)
-app.get('/favorite' , favouritePageHandler)
-app.get('/trending', trendingHandler);
-app.get('/searchMovies', searchMoviesHandler);
-app.use("*", notFoundHandler);
-app.use(errorHandler);
 
-const KEY = "9156d3294f952e6f7e07e6b1710bcf32";
+// read data from JSON file
+const data = require("./MovieData/data.json");
+
+// Get axios so we can send HTTP requests to an API
 
 
-axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${KEY}&language=en-US&query}`)
-    .then(apiResponse=> {
+// connect to DB, and init the Client
 
-        apiResponse.data.results.map(value => {
-            let oneResult = new Movie(value.id, value.title,value.release_date, value.poster_path, value.overview);
-            results.push(oneResult);
-        });
+//end initelizing variables
 
-        return res.status(200).json(results);
-    })
-    .catch(error => {
-        errorHandler(req, res,error);
-    })
-    function trendingHandler(request, response){
-        let result = [];
-        axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${KEY}&language=en-US`)
-        .then(apiResponse => {
-            apiResponse.data.results.map(value => {
-                let oneResult = new Movie(value.id, value.title,value.release_date, value.poster_path, value.overview);
-                result.push(oneResult);
-            });
-    
-            return response.status(200).json(result);
-    
-        }).catch(error => {
-            errorHandler(request, response, error);
-        });
-    }
+// To get the data from the body object, it should be upove the paths
 
-    function errorHandler(request, response, error){
 
-        const err = {
-            status : 500,
-            message : error
-        }
-    
-        return res.status(500).send(err);
-    }
+// GET: paths
+app.get("/" , MovieData);
+app.get("/favorite" , favoritePageHandler)
 
-function movieInfo(title,potser_path,overview){
-this.title = title;
-this.path = potser_path;
-this.overvew = overview;
-}
 
-function moviesHandler(req , res){
-    let arr = []
-let spiderMan = new movieInfo("Spider-Man: No Way Home","/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg","Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.");
-arr.push(spiderMan);
-return res.status(200).send(arr); 
-}
-function favouritePageHandler(request , response)
+
+// should be at the end
+app.use("*" , notFoundHandler)
+
+// POST: paths
+
+
+// functions
+    //constructor
+function MovieInfo(title,poster_path,overview)
 {
-    return response.send("Welcome to Favorite Page");
+    this.title = title;
+    this.poster_path = poster_path;
+    this.overview = overview;
 }
-app.listen(3000,() => {
-    console.log("Listen on 3000");
-});
+      //end constructor
+
+// function1
+function MovieData(request , response){
+    let specificInfo = new MovieInfo(data.title,data.poster_path,data.overview)
+return response.status(200).json(specificInfo)
+}
+// function2
+function favoritePageHandler(req, res){
+res.status(200).send("Welcome to Favorite Page")
+}
+//  function3
+function notFoundHandler(req, res){
+res.status(404).send("Not Found!")
+}
+
+
+// end functions
+app.listen(4444 , ()=>{
+  
+    console.log("You are in Port number 4444");
+
+})
